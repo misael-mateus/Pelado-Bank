8ef10d7f32863b6a00d65137578265923cf3fdd2
misael mateus <misaelmateus.java@gmail.com>
2022-11-04 11:49:30 -0300

    Subindo Proj

---
+++ src/main/java/br/ufms/pelado_bank/PeladoBankApplication.java
@@ -0,0 +1,13 @@
+package br.ufms.pelado_bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeladoBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeladoBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/ufms/pelado_bank/PeladoBankApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufms.pelado_bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeladoBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
